bec8ee1d16d3b856cde749be9422b4f907343957
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-17 13:40:03 +0100

    Crear clase Cliente

--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/Cliente.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/Cliente.java
@@ -1,5 +1,19 @@
 package org.cuatrovientos.dam.psp.simuladorCine;
 
 public class Cliente {
+	private final String nombre;
 
+	public Cliente(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nombre=" + nombre + "]";
+	}
 }
