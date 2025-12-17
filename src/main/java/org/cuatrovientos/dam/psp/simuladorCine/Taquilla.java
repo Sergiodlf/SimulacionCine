355663964d959800624b794535c0ea08b459cdd2
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-17 14:52:13 +0100

    Añadir lógica del hilo Taquilla

--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/Taquilla.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/Taquilla.java
@@ -1,10 +1,18 @@
 package org.cuatrovientos.dam.psp.simuladorCine;
 
+import java.time.Duration;
+import java.util.Random;
+
 public class Taquilla implements Runnable {
 
 	private String nombre;
 	private ColaCine cola;
 	private long timestampInicio = 0;
+    private Random random = new Random();
+
+    private static final Duration ESPERA_TAQUILLA = Duration.ofMillis(1000);
+    private static final int TIEMPO_MIN_VENTA = 20_000;
+    private static final int TIEMPO_MAX_VENTA = 30_000;
 
 	public Taquilla(String nombre) {
 		super();
@@ -13,7 +21,25 @@ public class Taquilla implements Runnable {
 
 	@Override
 	public void run() {
+		timestampInicio = System.currentTimeMillis();
 
+		try {
+			boolean taquillaAbierta = true;
+			while (taquillaAbierta) {
+				Cliente cliente = cola.recuperarSiguienteCliente();
+
+				if (cliente != null) {
+					log("Vendiendo entrada a " + cliente.getNombre());
+					Thread.sleep(TIEMPO_MIN_VENTA + random.nextInt(TIEMPO_MAX_VENTA - TIEMPO_MIN_VENTA));
+					log("Entrada vendida a " + cliente.getNombre());
+					cola.finalizarCliente(cliente);
+				} else {
+					Thread.sleep(ESPERA_TAQUILLA);
+				}
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 
 	public void asignarCola(ColaCine cola) {
