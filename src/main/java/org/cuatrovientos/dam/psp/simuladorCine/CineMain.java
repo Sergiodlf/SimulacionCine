ce2580ee4631a9fa00ddebac81ce0be4fbafdeca
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-17 13:34:33 +0100

    Preparar el Main

---
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/CineMain.java
@@ -0,0 +1,16 @@
+package org.cuatrovientos.dam.psp.simuladorCine;
+
+public class CineMain {
+
+	public static void main(String[] args) {
+		// TODO Crear cola, taquillas y generador de clientes
+		
+		// TODO Asignar la cola a las taquillas y el generador de clientes
+		
+		// TODO Iniciar los hilos
+		
+		// TODO Abrir el cine
+
+	}
+
+}
