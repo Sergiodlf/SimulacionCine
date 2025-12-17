ce10e761a89c5e3ae45e9663771907a46cff60a9
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-17 14:02:10 +0100

    Añadir métodos para gestionar los clientes en la ColaCine

--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/ColaCine.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/ColaCine.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class ColaCine {
 	private int asientosDisponibles;
 
+	// Listas para los estados de los clientes
 	private List<Cliente> clientesRecibidos;
 	private List<Cliente> clientesEnCola;
 	private List<Cliente> clientesEnAtencion;
@@ -20,6 +21,41 @@ public class ColaCine {
 		this.clientesEnAtencion = new ArrayList<>();
 		this.clientesAtendidos = new ArrayList<>();
 		this.clientesSinEntrada = new ArrayList<>();
+	}
+
+	// Método para añadir un cliente nuevo a la cola
+	public synchronized void agregarCliente(Cliente cliente) {
+		log("Llega a la cola cliente: " + cliente.getNombre());
+		clientesRecibidos.add(cliente);
+		clientesEnCola.add(cliente);
+	}
+
+	// Función para conseguir el siguiente cliente de la cola
+	public synchronized Cliente recuperarSiguienteCliente() {
+		if (clientesEnCola.isEmpty()) {
+			return null;
+		}
+
+		if (asientosDisponibles <= 0) {
+			Cliente c = clientesEnCola.remove(0);
+			clientesSinEntrada.add(c);
+			log("No hay asientos, cliente se queda sin entrada: " + c.getNombre());
+			return null;
+		}
+
+		Cliente cliente = clientesEnCola.remove(0);
+		clientesEnAtencion.add(cliente);
+		asientosDisponibles--;
+
+		log("Cliente pasa a taquilla: " + cliente.getNombre());
+		return cliente;
+	}
+
+	// Método para finalizar la venta de un cliente
+	public synchronized void finalizarCliente(Cliente cliente) {
+		clientesEnAtencion.remove(cliente);
+		clientesAtendidos.add(cliente);
+		log("Cliente atendido: " + cliente.getNombre());
 	}
 
 	private void log(String mensaje) {
