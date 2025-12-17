7c380fa267c60f747cd6417e5dad8608698eb914
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-17 13:49:56 +0100

    Añadir las listas de estados de la cola y estructura básica de ColaCine

--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/ColaCine.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/ColaCine.java
@@ -1,5 +1,35 @@
 package org.cuatrovientos.dam.psp.simuladorCine;
 
-public class ColaCine {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ColaCine {
+	private int asientosDisponibles;
+
+	private List<Cliente> clientesRecibidos;
+	private List<Cliente> clientesEnCola;
+	private List<Cliente> clientesEnAtencion;
+	private List<Cliente> clientesAtendidos;
+	private List<Cliente> clientesSinEntrada;
+
+	public ColaCine(int asientosDisponibles) {
+		super();
+		this.asientosDisponibles = asientosDisponibles;
+		this.clientesRecibidos = new ArrayList<>();
+		this.clientesEnCola = new ArrayList<>();
+		this.clientesEnAtencion = new ArrayList<>();
+		this.clientesAtendidos = new ArrayList<>();
+		this.clientesSinEntrada = new ArrayList<>();
+	}
+
+	private void log(String mensaje) {
+		System.out.println("[" + Thread.currentThread().threadId() + "][ COLA ] " + mensaje);
+	}
+
+	@Override
+	public synchronized String toString() {
+		return "ColaCine [ ColaUnica ] " + "Recibidos=" + clientesRecibidos.size() + ", EnCola=" + clientesEnCola.size()
+				+ ", Atendidos=" + clientesAtendidos.size() + ", SinEntrada=" + clientesSinEntrada.size()
+				+ ", AsientosRestantes=" + asientosDisponibles;
+	}
 }
