edb0928047d27dda45fd1f7d7dab6d6b7ce9a0f4
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-17 14:09:56 +0100

    Añadir estructura base de la clase Taquilla

--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/Taquilla.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/Taquilla.java
@@ -2,8 +2,26 @@ package org.cuatrovientos.dam.psp.simuladorCine;
 
 public class Taquilla implements Runnable {
 
+	private String nombre;
+	private ColaCine cola;
+	private long timestampInicio = 0;
+
+	public Taquilla(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
 	@Override
 	public void run() {
-		
+
+	}
+
+	public void asignarCola(ColaCine cola) {
+		this.cola = cola;
+	}
+
+	private void log(String mensaje) {
+		System.out.println("\t[" + Thread.currentThread().threadId() + "][" + nombre + "] "
+				+ (System.currentTimeMillis() - timestampInicio) + ": " + mensaje);
 	}
 }
