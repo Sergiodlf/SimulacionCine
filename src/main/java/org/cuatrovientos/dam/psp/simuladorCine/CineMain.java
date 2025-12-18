4d5ade1184e403aa811fa794956a559473d5b488
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-18 12:25:02 +0100

    Terminar estructura base + lógica del main, primera versión del programa finalizada

--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/CineMain.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/CineMain.java
@@ -1,16 +1,40 @@
 package org.cuatrovientos.dam.psp.simuladorCine;
 
+import java.time.Duration;
+
 public class CineMain {
 
-	public static void main(String[] args) {
-		// TODO Crear cola, taquillas y generador de clientes
-		
-		// TODO Asignar la cola a las taquillas y el generador de clientes
-		
-		// TODO Iniciar los hilos
-		
-		// TODO Abrir el cine
+	// DATOS CONFIGURABLES
+	private static final int ASIENTOS_DISPONIBLES = 200;
+	private static final Duration INTERVALO_MONITORIZACION = Duration.ofSeconds(10);
 
+	public static void main(String[] args) {
+		// Crear cola, taquillas y generador de clientes
+		ColaCine cola = new ColaCine(ASIENTOS_DISPONIBLES);
+		Taquilla taquilla1 = new Taquilla("Taquilla_1");
+		Taquilla taquilla2 = new Taquilla("Taquilla_2");
+		GeneradorClientes generador = new GeneradorClientes();
+
+		// Asignar la cola a las taquillas y el generador de clientes
+		taquilla1.asignarCola(cola);
+		taquilla2.asignarCola(cola);
+		generador.asignarCola(cola);
+
+		// Iniciar los hilos
+		new Thread(taquilla1).start();
+		new Thread(taquilla2).start();
+		new Thread(generador).start();
+
+		// Abrir el cine
+		try {
+			boolean cineAbierto = true;
+			while (cineAbierto) {
+				Thread.sleep(INTERVALO_MONITORIZACION);
+				System.out.println("[" + Thread.currentThread().threadId() + "][MAIN]: " + cola.toString());
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 
 }
--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/GeneradorClientes.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/GeneradorClientes.java
@@ -1,19 +1,37 @@
 package org.cuatrovientos.dam.psp.simuladorCine;
 
+import java.time.Duration;
+
 public class GeneradorClientes implements Runnable {
-	
+
 	private ColaCine cola;
 	private long timestampInicio = 0;
+	
+	private static final Duration ESPERA_NUEVO_CLIENTE = Duration.ofMillis(4000);
 
 	public GeneradorClientes() {
 		super();
 	}
-	
+
 	@Override
 	public void run() {
-		
+		timestampInicio = System.currentTimeMillis();
+		try {
+			int contador = 1;
+			boolean generadorEncendido = true;
+			while (generadorEncendido) {
+				Cliente cliente = new Cliente("Cliente_" + contador++);
+
+				log("Se manda a la cola nuevo cliente: " + cliente.getNombre());
+				cola.agregarCliente(cliente);
+
+				Thread.sleep(ESPERA_NUEVO_CLIENTE);
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
-	
+
 	public void asignarCola(ColaCine cola) {
 		this.cola = cola;
 	}
