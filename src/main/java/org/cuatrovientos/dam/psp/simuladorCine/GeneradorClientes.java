bd2d96ae7ac381f28e4620cbc5e6d73e54cecbae
Sergio de la Fuente <sergio4vientos@gmail.com>
2025-12-18 12:10:32 +0100

    Añadir estructura base + lógica del Generador de clientes

--- src/main/java/org/cuatrovientos/dam/psp/simuladorCine/GeneradorClientes.java
+++ src/main/java/org/cuatrovientos/dam/psp/simuladorCine/GeneradorClientes.java
@@ -2,8 +2,24 @@ package org.cuatrovientos.dam.psp.simuladorCine;
 
 public class GeneradorClientes implements Runnable {
 	
+	private ColaCine cola;
+	private long timestampInicio = 0;
+
+	public GeneradorClientes() {
+		super();
+	}
+	
 	@Override
 	public void run() {
 		
+	}
+	
+	public void asignarCola(ColaCine cola) {
+		this.cola = cola;
+	}
+
+	private void log(String mensaje) {
+		System.out.println("[" + Thread.currentThread().threadId() + "][ GeneradorClientes ] "
+				+ (System.currentTimeMillis() - timestampInicio) + ": " + mensaje);
 	}
 }
